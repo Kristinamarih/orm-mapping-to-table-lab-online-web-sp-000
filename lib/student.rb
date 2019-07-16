47633f3114c34a8ebacf679d0f04f124fdfefc70
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 10:20:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,7 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
   end
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
