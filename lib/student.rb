22f1ab161a609a1cf1ed930218f28942c3f182ac
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 10:19:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,8 @@ class Student
     sql = <<-SQL 
     INSERT INTO students (name, grade)
     VALUES (?, ?)
+    SQL
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
