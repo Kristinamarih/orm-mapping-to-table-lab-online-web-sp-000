25d6065c630586c6a3a33de763aea3e31af54d6a
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 10:17:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ class Student
   
   def save
     sql = <<-SQL 
-    
+    INSERT INTO students
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
